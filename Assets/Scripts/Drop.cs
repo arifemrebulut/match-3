13f39fb0a0e0902f4aaf7ea9c78371bf828ce344
Emre Bulut <arifemrebulut@gmail.com>
2023-10-30 17:08:39 +0300

    feat: drop pool created

--- Assets/Scripts/Drop.cs
+++ Assets/Scripts/Drop.cs
@@ -4,6 +4,29 @@ namespace Agave
 {
     public class Drop : MonoBehaviour
     {
-        public DropTypeSO Type { get; set; }
+        private SpriteRenderer _spriteRenderer;
+
+        private DropTypeSO _type;
+        
+        public DropTypeSO Type
+        {
+            get => _type;
+            set
+            {
+                _type = value;
+
+                if (_spriteRenderer == null)
+                {
+                    _spriteRenderer = GetComponent<SpriteRenderer>();
+                }
+                
+                _spriteRenderer.sprite = _type.image;
+            }
+        }
+
+        public void SetPosition(int x, int y)
+        {
+            transform.position = new Vector2(x, y);
+        }
     }
 }
---
+++ Assets/Scripts/DropPool.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Agave
+{
+    public class DropPool : ObjectPool<Drop>
+    {
+        [SerializeField] private List<DropTypeSO> dropsTypes;
+
+        public Drop GetRandomDrop()
+        {
+            var drop = base.GetPooledObject();
+        
+            return RandomizeDropType(drop);
+        }
+
+        public void ReturnDropToPool(Drop dropToReturn)
+        {
+            dropToReturn.transform.localScale = Vector3.one;
+        
+            base.ReturnObjectToPool(dropToReturn);
+        }
+
+        public DropTypeSO GetNextType(Drop dropToChangeType)
+        {
+            var typeIndex = dropsTypes.IndexOf(dropToChangeType.Type);
+            return dropsTypes[(typeIndex + 1) % dropsTypes.Count];
+        }
+
+        private Drop RandomizeDropType(Drop dropToRandomize)
+        {
+            var randomDropType = dropsTypes[Random.Range(0, dropsTypes.Count)];
+            dropToRandomize.Type = randomDropType;
+
+            return dropToRandomize;
+        }
+    }
+}
